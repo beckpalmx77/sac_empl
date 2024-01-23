4e0a1e9ddf7daea27e66885182e113f057147f65
beckpalmx77 <beckpalmx@gmail.com>
2024-01-23 09:46:05 +0700

    Update get_temperature.php

--- model/get_temperature.php
+++ model/get_temperature.php
@@ -1,17 +1,23 @@
 <?php
 include('../config/connect_db.php');
 
-$temp =  $_GET["temp"];
+$temp_c = $_GET["temp_c"];
+$temp_f = $_GET["temp_f"];
+$humidity = $_GET["humidity"];
+$heat_index = $_GET["heat_index"];
 
 $currentDate = date('Y-m-d H:i:s');
 
 $status = 'N';
 
-$sql = "INSERT INTO temperature_data(date_time,temperature_c,status) 
-            VALUES (:date_time,:temperature_c,:status)";
+$sql = "INSERT INTO temperature_data(date_time,temperature_c,temperature_f,humidity,heat_index,status) 
+            VALUES (:date_time,:temperature_c,:temperature_f,:humidity,:heat_index,:status)";
 $query = $conn->prepare($sql);
 $query->bindParam(':date_time', $currentDate, PDO::PARAM_STR);
-$query->bindParam(':temperature_c', $temp, PDO::PARAM_STR);
+$query->bindParam(':temperature_c', $temp_c, PDO::PARAM_STR);
+$query->bindParam(':temperature_f', $temp_f, PDO::PARAM_STR);
+$query->bindParam(':humidity', $humidity, PDO::PARAM_STR);
+$query->bindParam(':heat_index', $heat_index, PDO::PARAM_STR);
 $query->bindParam(':status', $status, PDO::PARAM_STR);
 $query->execute();
 $lastInsertId = $conn->lastInsertId();
