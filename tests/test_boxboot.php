8e65d214a0cf1be021750d83309cd99273399e6f
beckpalmx77 <beckpalmx@gmail.com>
2024-06-17 15:41:43 +0700

    Update test_boxboot.php

--- tests/test_boxboot.php
+++ tests/test_boxboot.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <title>My page</title>
 
     <!-- CSS dependencies -->
@@ -13,7 +13,7 @@
 
 <!-- JS dependencies -->
 <script
-    src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <!-- Bootstrap 4+ dependency -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
 <script src="../bootstrap/js/bootstrap.min.js"></script>
@@ -21,8 +21,8 @@
 <!-- bootbox code -->
 <script src="../js/bootbox/bootbox.all.js"></script>
 <script>
-    $(document).on('click', '.show-alert', function(e) {
-        bootbox.alert('Hello world!', function() {
+    $(document).on('click', '.show-alert', function (e) {
+        bootbox.alert('Hello world!', function () {
             console.log('Alert Callback');
         });
     });
